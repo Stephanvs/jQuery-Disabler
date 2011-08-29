c97f265089129f7b37da1995592549da594853b8
Gijs van de Nieuwegiessen <gijs@d1.nl>
2011-08-29 14:06:06 +0200

    first commit

---
+++ README
@@ -0,0 +1,10 @@
+# jQuery Disabler
+
+This tiny plugin makes it easy to disable any input controls or anchor tags.
+
+		// Call disable on a selector and recursively disable
+		// all input controls and anchor tags
+		$(selector).disable();
+
+		// Call enable to enable 
+		$(selector).enable();
---
+++ jquery-disabler-1.0.0.js
@@ -0,0 +1,40 @@
+﻿// Disable or enable controls
+(function ($) {
+    // Visual state
+    // 
+    // Usage:
+    // - Disable the element and all childres
+    // $(selector).disable();
+    jQuery.fn.disable = function (disable) {
+    
+        if ($(this).length == 0) return;
+
+        var controls = $(this).filter('input,select,a');
+        if (controls.length == 0) {
+            controls = $(this).find('input,select,a');
+        }
+
+        if (disable === undefined || disable) {
+            controls.filter('a').addClass('disabled');
+            controls.attr('disabled', 'true');
+            controls.bindFirst('click', DisableClick);
+        }
+        else {
+            controls.filter('a').removeClass('disabled');
+            controls.removeAttr('disabled');
+            controls.unbind('click', DisableClick);
+        }
+
+        return this;
+    };
+
+    jQuery.fn.enable = function (disable) {
+        return $(this).disable(false);
+    };
+
+    // Click handler for <a> tags
+    function DisableClick(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    };
+})(jQuery);
