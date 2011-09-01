8ab9da369f51281f0c4e78c56691be5ce2822195
Gijs van de Nieuwegiessen <gijs@d1.nl>
2011-09-01 17:23:21 +0200

    Fixed specific selector conditions

---
+++ jquery-disabler-1.0.1.js
@@ -0,0 +1,40 @@
+﻿// Disable or enable controls
+(function ($) {
+    // Visual state
+    // 
+    // Usage:
+    // - Disable the element and all childres
+    // $(selector).disable();
+    jQuery.fn.disable = function (disable) {
+
+        if (jQuery(this).length == 0) return;
+
+        var controls = jQuery(this).filter('input,select,a').add(jQuery(this).find('input,select,a'));
+        if (controls.length == 0) {
+            return this;
+        }
+
+        if (disable === undefined || disable) {
+            controls.filter('a').addClass('disabled');
+            controls.attr('disabled', 'true');
+            controls.bindFirst('click', DisableClick);
+        }
+        else {
+            controls.filter('a').removeClass('disabled');
+            controls.removeAttr('disabled');
+            controls.unbind('click', DisableClick);
+        }
+
+        return this;
+    };
+
+    jQuery.fn.enable = function (disable) {
+        return jQuery(this).disable(false);
+    };
+
+    // Click handler for <a> tags
+    function DisableClick(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    };
+})(jQuery);
